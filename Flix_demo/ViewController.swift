2f35d6cf205b81836a8168d76fdfe57ba0f4cd88
JunYu Cao <davidcao2115@gmail.com>
2018-09-25 17:59:23 -0400

    Initial Commit

---
+++ Flix_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix_demo
+//
+//  Created by David Cao on 9/25/18.
+//  Copyright © 2018 David Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
